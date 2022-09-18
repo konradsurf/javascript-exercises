231b37dff894f9ab25a19cc7510a2f6f5c7dd72c
konradkarwowski <konrad.bipc@gmail.com>
2022-09-18 08:34:31 -0700

    completed repeatString with all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let repeatingString = string;
+  if (num == 0) {
+    repeatingString = '';
+  }
+  else if (num < 0) {
+    repeatingString = 'ERROR';
+  }
+  for (let i = 1; i < num; i++){
+    repeatingString += string;
+  }
+  return repeatingString;
 };
 
 // Do not edit below this line
