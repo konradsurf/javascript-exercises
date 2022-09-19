79504f8488e235bc6c2009bad7d285cf4cc1d00d
konradkarwowski <konrad.bipc@gmail.com>
2022-09-19 11:58:14 -0700

    Completed assignment and passed all tests Found out I don't know how to debug an NPM Test file using VS code's debugger.  Literally clueless - set reminder to learn this later this week.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,31 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  const excludedElements = args;
+  // for (let i = 0; )
+  
+  for (let i = 0; i < excludedElements.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (excludedElements[i] === array[j]) {
+        array.splice(j, 1);
+      }
+    }
+  }
 
+
+  // for (let i = 0; i < array.length; i++){
+  //   for (let j = 0; j < excludedElements.length; j++) {
+  //     if (array[i] == excludedElements[j]) {
+  //       array.splice(i, 1);
+  //     }
+  //   }
+  // }
+  
+  // for (let i = 0; i < array.length; i++) {
+  //   if (array[i] == x){
+  //     array.splice(i, 1);
+  //   }
+  // }
+  return array;
+  
 };
 
 // Do not edit below this line
