6e972359200ec7a023253f4ef88226046058f24a
konradkarwowski <konrad.bipc@gmail.com>
2022-09-21 14:11:39 -0700

    Finished sumAll exercise. Passed all tests. Displaying an error message when argument wasn't a number was the most challenging part. I got stuck on using isNaN and Number.isNan() , but then realized i could just use typeof in order to determine if argument passed was a number before any coercion that would change string of digits into a number.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+  let sum = 0;
+  if (numOne < 0 || numTwo < 0){
+    sum = 'ERROR';
+  }
+  else if (typeof numOne != 'number' || typeof numTwo != 'number'){
+    sum = 'ERROR';
+  } 
+  else {
+    for (i = Math.min(numOne, numTwo); i <= Math.max(numOne, numTwo); i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
