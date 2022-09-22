5b78cc27f4713bcdce9696e72c9aa2ea70816d84
konradkarwowski <konrad.bipc@gmail.com>
2022-09-22 10:38:45 -0700

    Completed assignment, passed all tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function(tempInFrnht) {
+  let tempInClcs = (tempInFrnht - 32) * (5 / 9);
+  return ((Math.round(tempInClcs * 10)) / 10); 
 
 };
 
-const ctof = function() {
-
+const ctof = function(tempInClcs) {
+  let tempInFrnht = (tempInClcs * 9 / 5) + 32;
+  return ((Math.round(tempInFrnht * 10 )) / 10); 
 };
 
 // Do not edit below this line
