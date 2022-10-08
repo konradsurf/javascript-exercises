98090a9fba69d7ce76c290bd59188403b7538303
konradkarwowski <konrad.bipc@gmail.com>
2022-10-08 14:35:53 -0700

    Added notes regarding TOP's method using reverse method vs my for loop.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -6,5 +6,12 @@ const reverseString = function(string) {
   return newString;
 };
 
+/* 
+rather than using a for loop, TOP's solution used a reverse method
+(in addition to split and join).  The reverse method is a an array
+method, but the split method returns an array that the reverse
+method was used on
+*/
+
 // Do not edit below this line
 module.exports = reverseString;
