f389f305fec2ce0305d9999207b20142feeb1d5a
konradkarwowski <konrad.bipc@gmail.com>
2022-10-08 14:07:43 -0700

    Added notes after reviewing TOP's solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -6,11 +6,21 @@ const repeatString = function(string, num) {
   else if (num < 0) {
     repeatingString = 'ERROR';
   }
-  for (let i = 1; i < num; i++){
-    repeatingString += string;
+// this only works when num =1 b/c i assigned string argument to repeatString variable - see TOP solution below
+  for (let i = 1; i < num; i++){    
+      repeatingString += string;
   }
   return repeatingString;
 };
+
+// const repeatString = function(word, times) {
+//   if (times < 0) return 'ERROR';
+//   let string = '';
+//   for (let i = 0; i < times; i++) {  
+//     string += word;
+//   }
+//   return string;
+// };
 
 // Do not edit below this line
 module.exports = repeatString;
